48fc3c348d75b031335c75acc14d9b82061885c6
Anchor79 <1905203787@qq.com>
2026-08-20 19:19:37 +0800

    Initialize Supabase client with URL and key

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,8 @@
+const SUPABASE_URL = "https://geclozmyjqmnokkqprho.supabase.co";
 
+const SUPABASE_KEY = "sb_publishable_ipiRMQgZj8Ctt33nnXDlew_c1HJlK-c";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
